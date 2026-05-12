f3468772b4f33a83feca09b38fc94be73d8ee0e7
kaushikrajiv9-arch <kaushikrajiv9@gmail.com>
2026-05-12 16:26:31 -0400

    feat: wire US stock feed + snapshot on stock-ws connect

--- artifacts/api-server/src/index.ts
+++ artifacts/api-server/src/index.ts
@@ -1,5 +1,16 @@
+import http from "http";
+import { WebSocketServer } from "ws";
 import app from "./app";
 import { logger } from "./lib/logger";
+import { addBrowserClient, sendCbSnapshot, shutdown as shutdownCoinbase } from "./lib/cbWsFeed";
+import { startKrakenFeed, shutdownKraken, sendKrakenSnapshot } from "./lib/krakenWsFeed";
+import { startOkxFeed, shutdownOkx, sendOkxSnapshot } from "./lib/okxWsFeed";
+import { startFundingRateFeed, stopFundingRateFeed, sendFundingRateSnapshot } from "./lib/fundingRateFeed";
+import { addStockClient, stopAlpacaFeed, broadcastStock } from "./lib/alpacaFeed";
+import { startBinanceFeed, shutdownBinance, sendBinanceSnapshot } from "./lib/binanceWsFeed";
+import { startBybitFeed, shutdownBybit, sendBybitSnapshot } from "./lib/bybitWsFeed";
+import { startNseFeed, stopNseFeed, sendNseSnapshot } from "./lib/nseStockFeed";
+import { startUsStockFeed, stopUsStockFeed, sendUsStockSnapshot } from "./lib/usStockFeed";
 
 const rawPort = process.env["PORT"];
 
@@ -15,11 +26,58 @@ if (Number.isNaN(port) || port <= 0) {
   throw new Error(`Invalid PORT value: "${rawPort}"`);
 }
 
-app.listen(port, (err) => {
-  if (err) {
-    logger.error({ err }, "Error listening on port");
-    process.exit(1);
-  }
+const server = http.createServer(app);
+const wss = new WebSocketServer({ noServer: true });
 
+server.on("upgrade", (req, socket, head) => {
+  const pathname = new URL(req.url ?? "/", `http://${req.headers.host}`)
+    .pathname;
+  if (pathname === "/api/ws") {
+    wss.handleUpgrade(req, socket, head, (ws) => {
+      addBrowserClient(ws);
+      sendCbSnapshot(ws);
+      sendKrakenSnapshot(ws);
+      sendOkxSnapshot(ws);
+      sendFundingRateSnapshot(ws);
+      sendBinanceSnapshot(ws);
+      sendBybitSnapshot(ws);
+    });
+  } else if (pathname === "/api/stock-ws") {
+    wss.handleUpgrade(req, socket, head, (ws) => {
+      addStockClient(ws);
+      const send = (msg: string) => { if ((ws as any).readyState === 1) ws.send(msg); };
+      sendNseSnapshot(send);
+      sendUsStockSnapshot(send);
+    });
+  } else {
+    socket.destroy();
+  }
+});
+
+wss.on("error", (err) => {
+  logger.error({ err }, "WebSocketServer error");
+});
+
+process.on("SIGTERM", () => {
+  shutdownCoinbase();
+  shutdownKraken();
+  shutdownOkx();
+  shutdownBinance();
+  shutdownBybit();
+  stopFundingRateFeed();
+  stopAlpacaFeed();
+  stopNseFeed();
+  stopUsStockFeed();
+  server.close(() => process.exit(0));
+});
+
+server.listen(port, () => {
   logger.info({ port }, "Server listening");
+  startKrakenFeed();
+  startOkxFeed();
+  startFundingRateFeed();
+  startBinanceFeed();
+  startBybitFeed();
+  startNseFeed((data) => broadcastStock(data));
+  startUsStockFeed((data) => broadcastStock(data));
 });
